1f559aff88ecba5ff4ae696204a6c908292f9896
Tripulantes-Mision-Tic <90871557+Tripulantes-Mision-Tic@users.noreply.github.com>
2021-09-24 19:34:43 -0500

    funcion para validar edad del usuario (#4)
    Co-authored-by: Marcos Correa <marcoscinturon@gmail.com>

--- validaciones.js
+++ validaciones.js
@@ -3,7 +3,11 @@ function validar_nombre_usuario(string){
 }
 
 function validar_edad_usuario(edad){
-
+    if (Number.isInteger(edad) && edad>=13 && edad<110){
+        return "verdadero"
+    }else{
+        return "falso"
+    }
 }
 
 function validar_contrasena(string){
