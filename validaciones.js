dcbdaf84c3fa773530468179ce4708c2254d9254
Tripulantes-Mision-Tic <90871557+Tripulantes-Mision-Tic@users.noreply.github.com>
2021-09-25 19:23:09 -0500

    Validar contraseña (#9)
    * subida de codigo para validar contraseña
    
    * vais
    
    Co-authored-by: Michael <palacioshmichaela@gmail.com>

--- validaciones.js
+++ validaciones.js
@@ -1,15 +1,37 @@
+console.log("aaaaaaaaaaaaaaaaaaaaaa");
+
 function validar_nombre_usuario(string){
 
 }
 
 function validar_edad_usuario(edad){
     if (Number.isInteger(edad) && edad>=13 && edad<110){
-        return "verdadero"
+        return "verdadero";
     }else{
-        return "falso"
+        return "falso";
     }
 }
 
-function validar_contrasena(string){
-    
+
+// =========== Validar Contraseña ===========
+
+function validar_contraseña(string){
+    // var contraseña = document.getElementById("dato_contrasena").value;
+
+    var caracteres = /^[a-z0-9]+$/i;
+    var validar = caracteres.test(string);
+
+    if (!validar){
+        alert("solo se permiten caracteres alfanumericos");
+        return validar;
+    }
+    else if (string.length > 6 ){
+        alert("solo se permiten maximo 6 caracteres");
+        return !validar;
+    }
+    else{
+        alert("contraseña correcta");
+        return validar;
+    }
+ 
 }
